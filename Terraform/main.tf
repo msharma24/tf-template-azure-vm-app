c7ee5eb968e2421c754540597388fa91ffc8cf40
Mukesh Sharma <mukeshsharma24@gmail.com>
2024-03-08 10:43:59 +1300

    updated nsg

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_network_security_group" "publc_nsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "80"
+    destination_port_range     = "*"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
